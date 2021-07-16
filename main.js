36c79965e8ad5fc0fe2eae13f7c8de90ef08a9fc
BhavayJain <77540919+BhavayJain@users.noreply.github.com>
2021-07-16 20:43:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ function setup(){
     classifier.on('pose',gotPoses);
 
 }
+noseX=0;
+noseY=0;
+leftWristx=0;
+rightWristx=0;
+difference=0;
 
 function modelLoaded(){
     console.log("modelLoaded");
@@ -15,9 +20,25 @@ function modelLoaded(){
 
 function draw(){
     background('#969A97');
+    fill('#590093');
+    text("Bhavay",noseX,noseY);
+    textSize(difference);
+    
 }
 
 
 function gotPoses(results){
     console.log(results);
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log(" noseX="+ noseX +" noseY =" + noseY);
+
+        leftWristx=results[0].pose.leftWrist.x;
+        rightWristx=results[0].pose.rightWrist.x;
+        difference= floor(leftWristx-rightWristx);
+        console.log(difference);
+        console.log(" leftWristx="+ leftWristx +" rightWristx =" + rightWristx);
+    }
 }
